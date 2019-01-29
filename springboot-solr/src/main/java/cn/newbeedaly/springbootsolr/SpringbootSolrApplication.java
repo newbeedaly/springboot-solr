578a3af997985638f82ebd3fc48014f4df5d5dd8
liqk <newbeedaly@163.com>
2019-01-29 17:11:32 +0800

    first commit
    首次提交，包含增删改查

---
+++ springboot-solr/src/main/java/cn/newbeedaly/springbootsolr/SpringbootSolrApplication.java
@@ -0,0 +1,14 @@
+package cn.newbeedaly.springbootsolr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSolrApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootSolrApplication.class, args);
+  }
+
+}
+
